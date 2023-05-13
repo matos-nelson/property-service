115fde8c8329409d445b1ae7c214aeebe32e6c8d
matos-nelson <nmatos6@gatech.edu>
2023-05-13 13:55:15 -0500

    Initial baseline commit.

---
+++ src/test/java/org/rent/circle/owner/api/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.rent.circle.owner.api;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
